1545667adaaeb501e09e9f54117efd3c3eca2431
Dave Farinelli <d@dfar.io>
2020-12-29 13:32:59 -0500

    Upgrade to v3 Progress

--- api/SwaggerUI.cs
+++ api/SwaggerUI.cs
@@ -18,7 +18,7 @@ namespace HD.Journally
     [FunctionName("SwaggerUI")]
     [SwaggerIgnore]
     public static Task<HttpResponseMessage> Run(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "/")]
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "")]
             HttpRequestMessage req,
         [SwashBuckleClient] ISwashBuckleClient swashBuckleClient)
     {
